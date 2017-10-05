239968758c474ef31dca04f98cb6d5e9b71990c1
Ryan Padilha <ryan.padilha@gmail.com>
2017-10-05 14:08:14 -0300

    uber-deploy

--- src/main/resources/bin/start.sh
+++ src/main/resources/bin/start.sh
@@ -1,7 +1,7 @@
-ENV=$1
-if [ $ENV == '' ]
-then
-$ENV = "dev";
+if [ -z $1 ]; then
+  ENV="dev";
+else
+  ENV=$1
 fi
 
 java -Xms256m -Xmx512m -Dlogging.config=../config/logback.xml -Dspring.config.location=../config/application-$ENV.properties -jar ../lib/wplex-garagem.jar
