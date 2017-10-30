42f972e34e836079ef94ca3c69e6ecbdc3fad3ad
Ryan Padilha <ryan.padilha@gmail.com>
2017-10-30 20:15:30 -0200

    start-stop shell files

--- src/main/resources/bin/start.sh
+++ src/main/resources/bin/start.sh
@@ -13,6 +13,9 @@ APPAWSPATH="/var/wplex/apps/wplex-garagem"
 echo "starting wplex-garagem application..."
 
 BUILD_ID=dontKillMe nohup nice java -Xms256m -Xmx512m -Dlogging.config=$APPAWSPATH/config/logback-spring.xml -Dspring.config.location=$APPAWSPATH/config/application-$ENV.properties -jar $APPAWSPATH/lib/wplex-garagem.jar &
-# echo $! > /var/run/wplex-garagem.pid
+echo $! > /var/run/wplex-garagem.pid
+
+echo "wplex-application started."
+exit
 
 $SHELL
